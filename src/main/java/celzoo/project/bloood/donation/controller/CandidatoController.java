ab70841ce81e95b8636bbaedf27b2f367ced8494
celzo_aveiro <celzoismaelaveirofilho@gmail.com>
2024-10-15 11:03:08 -0300

    Commitando o spring security

--- src/main/java/celzoo/project/bloood/donation/controller/CandidatoController.java
+++ src/main/java/celzoo/project/bloood/donation/controller/CandidatoController.java
@@ -4,6 +4,7 @@ import celzoo.project.bloood.donation.model.Candidato;
 import celzoo.project.bloood.donation.service.CandidatoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,33 +18,40 @@ public class CandidatoController {
     @Autowired
     private CandidatoService candidatoService;
 
+
     @PostMapping("/importar")
+    @PreAuthorize("hasAuthority('ROLE_USER')") // Exemplo: exige que o usuário tenha a ROLE_USER
     public ResponseEntity<Void> importarCandidatos(@RequestBody List<Candidato> candidatos) {
         candidatoService.importarCandidatos(candidatos);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/contarPorEstado")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public Map<String, Long> contarCandidatosPorEstado() {
         return candidatoService.contarCandidatosPorEstado();
     }
 
     @GetMapping("/recuperarImcMedio")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public Map<Integer, Double> recuperaImcMedio() {
         return candidatoService.calcularIMCMedioPorFaixaDeIdade();
     }
 
     @GetMapping("/recuperarPercentualDeObesosPorGenero")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public Map<String, Double> recuperaObesidadePorSexo() {
         return candidatoService.calcularPercentualObesosPorSexo();
     }
 
     @GetMapping("/recuperarMediaDeIdadePorTipoSanguineo")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public Map<String, Double> recuperaMediaDeIdadePorTipoSanguineo() {
         return candidatoService.calcularMediaIdadePorTipoSanguineo();
     }
 
     @GetMapping("/recuperarDoadoresPorTipoSanguineo")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public Map<String, Long> recuperaDoadoresPorTipoSanguineo() {
         return candidatoService.contarDoadoresPorTipoSanguineo();
     }
